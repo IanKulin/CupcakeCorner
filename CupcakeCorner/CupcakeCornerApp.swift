6f1856a18a84167022e443b7e9ca5d83898a84c9
Ian Bailey <IanKulin@kulin.com.au>
2022-10-28 07:16:25 +0800

    Initial

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ian Bailey on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
